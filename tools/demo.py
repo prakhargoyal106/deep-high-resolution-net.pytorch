de4901d73e93669eec03713596d8bee65ff055ac
prakhargoyal106 <52134165+prakhargoyal106@users.noreply.github.com>
2020-09-21 13:56:48 +0530

    Update demo.py

--- tools/demo.py
+++ tools/demo.py
@@ -1,217 +1,166 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 import argparse
-import datetime
-import json
-import random
-import time
-from pathlib import Path
-
-import numpy as np
+import os
+import pprint
 import torch
-from torch.utils.data import DataLoader, DistributedSampler
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+import _init_paths
+from config import cfg
+from config import update_config
+from core.loss import JointsMSELoss
+from core.function import validate, get_final_preds
+from utils.utils import create_logger
+from utils.transforms import *
+import cv2
+import dataset
+import models
+import numpy as np
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train keypoints network')
+    # general
+    parser.add_argument('--cfg',
+                        help='experiment configure file name',
+                        default='experiments/mpii/hrnet/w32_256x256_adam_lr1e-3.yaml',
+                        type=str)
 
-import datasets
-import util.misc as utils
-from datasets import build_dataset, get_coco_api_from_dataset
-from engine import evaluate, train_one_epoch
-from models import build_model
+    parser.add_argument('opts',
+                        help="Modify config options using the command-line",
+                        default=None,
+                        nargs=argparse.REMAINDER)
 
+    parser.add_argument('--img-file',
+                        help='input your test img',
+                        type=str,
+                        default='')
+    # philly
+    parser.add_argument('--modelDir',
+                        help='model directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--logDir',
+                        help='log directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--dataDir',
+                        help='data directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--prevModelDir',
+                        help='prev Model directory',
+                        type=str,
+                        default='')
+    args = parser.parse_args()
+    return args
 
-import pytorch_lightning as pl
-
-
-
-def get_args_parser():
-    parser = argparse.ArgumentParser('Set transformer detector', add_help=False)
-    parser.add_argument('--lr', default=1e-4, type=float)
-    parser.add_argument('--lr_backbone', default=1e-5, type=float)
-    parser.add_argument('--batch_size', default=2, type=int)
-    parser.add_argument('--weight_decay', default=1e-4, type=float)
-    parser.add_argument('--epochs', default=300, type=int)
-    parser.add_argument('--lr_drop', default=200, type=int)
-    parser.add_argument('--clip_max_norm', default=0.1, type=float,
-                        help='gradient clipping max norm')
-
-    # Model parameters
-    parser.add_argument('--frozen_weights', type=str, default=None,
-                        help="Path to the pretrained model. If set, only the mask head will be trained")
-    # * Backbone
-    parser.add_argument('--backbone', default='resnet50', type=str,
-                        help="Name of the convolutional backbone to use")
-    parser.add_argument('--dilation', action='store_true',
-                        help="If true, we replace stride with dilation in the last convolutional block (DC5)")
-    parser.add_argument('--position_embedding', default='sine', type=str, choices=('sine', 'learned'),
-                        help="Type of positional embedding to use on top of the image features")
-
-    # * Transformer
-    parser.add_argument('--enc_layers', default=6, type=int,
-                        help="Number of encoding layers in the transformer")
-    parser.add_argument('--dec_layers', default=6, type=int,
-                        help="Number of decoding layers in the transformer")
-    parser.add_argument('--dim_feedforward', default=2048, type=int,
-                        help="Intermediate size of the feedforward layers in the transformer blocks")
-    parser.add_argument('--hidden_dim', default=256, type=int,
-                        help="Size of the embeddings (dimension of the transformer)")
-    parser.add_argument('--dropout', default=0.1, type=float,
-                        help="Dropout applied in the transformer")
-    parser.add_argument('--nheads', default=8, type=int,
-                        help="Number of attention heads inside the transformer's attentions")
-    parser.add_argument('--num_queries', default=100, type=int,
-                        help="Number of query slots")
-    parser.add_argument('--pre_norm', action='store_true')
-
-    # * Segmentation
-    parser.add_argument('--masks', action='store_true',
-                        help="Train segmentation head if the flag is provided")
-
-    # Loss
-    parser.add_argument('--no_aux_loss', dest='aux_loss', action='store_false',
-                        help="Disables auxiliary decoding losses (loss at each layer)")
-    # * Matcher
-    parser.add_argument('--set_cost_class', default=1, type=float,
-                        help="Class coefficient in the matching cost")
-    parser.add_argument('--set_cost_bbox', default=5, type=float,
-                        help="L1 box coefficient in the matching cost")
-    parser.add_argument('--set_cost_giou', default=2, type=float,
-                        help="giou box coefficient in the matching cost")
-    # * Loss coefficients
-    parser.add_argument('--mask_loss_coef', default=1, type=float)
-    parser.add_argument('--dice_loss_coef', default=1, type=float)
-    parser.add_argument('--bbox_loss_coef', default=5, type=float)
-    parser.add_argument('--giou_loss_coef', default=2, type=float)
-    parser.add_argument('--eos_coef', default=0.1, type=float,
-                        help="Relative classification weight of the no-object class")
-
-    # dataset parameters
-    parser.add_argument('--dataset_file', default='coco')
-    parser.add_argument('--coco_path', type=str)
-    parser.add_argument('--coco_panoptic_path', type=str)
-    parser.add_argument('--remove_difficult', action='store_true')
-
-    parser.add_argument('--output_dir', default='',
-                        help='path where to save, empty for no saving')
-    parser.add_argument('--device', default='cuda',
-                        help='device to use for training / testing')
-    parser.add_argument('--seed', default=42, type=int)
-    parser.add_argument('--resume', default='', help='resume from checkpoint')
-    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
-                        help='start epoch')
-    parser.add_argument('--eval', action='store_true')
-    parser.add_argument('--num_workers', default=2, type=int)
-
-    # distributed training parameters
-    parser.add_argument('--world_size', default=1, type=int,
-                        help='number of distributed processes')
-    parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
-    return parser
-
-class Model(pl.Module):
-    def __init__(self,model):
-        self.model = model
-    
-    def forward(self,x):
-        return self.model(x)
-    
+def _box2cs(box, image_width, image_height):
+    x, y, w, h = box[:4]
+    return _xywh2cs(x, y, w, h, image_width, image_height)
 
 
-class DetrDataModule(pl.LightningModule):
-    def __init__(self,args):
-        super().__init__()
-        self.args  = args
-    
-    def prepare_data(self):
-        dataset_train = build_dataset(image_set='train', args=self.args)
-        dataset_val = build_dataset(image_set='val', args=self.args)
+def _xywh2cs(x, y, w, h, image_width, image_height):
+    center = np.zeros((2), dtype=np.float32)
+    center[0] = x + w * 0.5
+    center[1] = y + h * 0.5
 
-        if self.args.distributed:
+    aspect_ratio = image_width * 1.0 / image_height
+    pixel_std = 200
 
-            self.sampler_train = DistributedSampler(dataset_train)
-            self.sampler_val = DistributedSampler(dataset_val, shuffle=False)
-        else:
-            self.sampler_train = torch.utils.data.RandomSampler(dataset_train)
-            self.sampler_val = torch.utils.data.SequentialSampler(dataset_val)
+    if w > aspect_ratio * h:
+        h = w * 1.0 / aspect_ratio
+    elif w < aspect_ratio * h:
+        w = h * aspect_ratio
+    scale = np.array(
+        [w * 1.0 / pixel_std, h * 1.0 / pixel_std],
+        dtype=np.float32)
+    if center[0] != -1:
+        scale = scale * 1.25
 
-        self.batch_sampler_train = torch.utils.data.BatchSampler(
-                                             self.sampler_train, self.args.batch_size, drop_last=True)
+    return center, scale
 
-    def setup(self):
-        self.dataset_train = build_dataset(image_set='train', args=self.args)
-        self.dataset_val = build_dataset(image_set='val', args=self.args)
+def main():
+    args = parse_args()
+    update_config(cfg, args)
 
+    logger, final_output_dir, tb_log_dir = create_logger(
+        cfg, args.cfg, 'valid')
 
-    def train_dataloader(self):
-        return DataLoader(self.dataset_train, batch_sampler=self.batch_sampler_train,
-                                   collate_fn=utils.collate_fn, num_workers=self.args.num_workers)
+    logger.info(pprint.pformat(args))
+    logger.info(cfg)
 
-    def val_dataloader(self):
-        return  DataLoader(self.dataset_val, self.args.batch_size, sampler=self.sampler_val,
-                                 drop_last=False, collate_fn=utils.collate_fn, num_workers=self.args.num_workers)
+    # cudnn related setting
+    cudnn.benchmark = cfg.CUDNN.BENCHMARK
+    torch.backends.cudnn.deterministic = cfg.CUDNN.DETERMINISTIC
+    torch.backends.cudnn.enabled = cfg.CUDNN.ENABLED
 
+    model = eval('models.'+cfg.MODEL.NAME+'.get_pose_net')(
+        cfg, is_train=False
+    )
 
+    if cfg.TEST.MODEL_FILE:
+        logger.info('=> loading model from {}'.format(cfg.TEST.MODEL_FILE))
+        model.load_state_dict(torch.load(cfg.TEST.MODEL_FILE), strict=False)
+    else:
+        model_state_file = os.path.join(
+            final_output_dir, 'final_state.pth'
+        )
+        logger.info('=> loading model from {}'.format(model_state_file))
+        model.load_state_dict(torch.load(model_state_file))
 
+    model = torch.nn.DataParallel(model, device_ids=cfg.GPUS).cuda()
 
-class detrClassifier(pl.LightningModule):
+    # define loss function (criterion) and optimizer
+    criterion = JointsMSELoss(
+        use_target_weight=cfg.LOSS.USE_TARGET_WEIGHT
+    ).cuda()
 
-    def __init__(self,model,criterion,postprocessors,args):
-        self.model = model
-        self.criterion=criterion
-        self.model_without_ddp = model
-        if  args.distrubuted:
-            self.model_without_ddp = model.module
-        
+    # Loading an image
+    image_file = args.img_file
+    data_numpy = cv2.imread(image_file, cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION)
+    if data_numpy is None:
+        logger.error('=> fail to read {}'.format(image_file))
+        raise ValueError('=> fail to read {}'.format(image_file))
 
+    # object detection box
+    box = [450, 160, 350, 560]
+    c, s = _box2cs(box, cfg.MODEL.IMAGE_SIZE[0], cfg.MODEL.IMAGE_SIZE[1])
+    r = 0
 
-        self.param_dicts = [
-                                {"params": [p for n, p in self.model_without_ddp.named_parameters() if "backbone" not in n and p.requires_grad]},
-                                {"params": [p for n, p in self.model_without_ddp.named_parameters() if "backbone" in n and p.requires_grad],
-                                 "lr": args.lr_backbone,},
-                          ]
+    trans = get_affine_transform(c, s, r, cfg.MODEL.IMAGE_SIZE)
+    input = cv2.warpAffine(
+        data_numpy,
+        trans,
+        (int(cfg.MODEL.IMAGE_SIZE[0]), int(cfg.MODEL.IMAGE_SIZE[1])),
+        flags=cv2.INTER_LINEAR)
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]),
+    ])
 
-    def training_step(self, batch, batch_idx):
-        
-        #metric_logger = utils.MetricLogger(delimiter=" ")
-        #metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
-        #metric_logger.add_meter('class_error', utils.SmoothedValue(window_size=1, fmt='{value:.2f}'))
+    input = transform(input).unsqueeze(0)
+    # switch to evaluate mode
+    model.eval()
+    with torch.no_grad():
+        # compute output heatmap
+        output = model(input)
+        preds, maxvals = get_final_preds(cfg, output.clone().cpu().numpy(), np.asarray([c]), np.asarray([s]))
 
-        samples,targets = batch
-        outputs = self.model(samples)
-        loss_dict = self.criterion(outputs,targets)
-        weight_dict = self.criterion.weight_dict
-        losses = sum(loss_dict[k] * weight_dict[k] for k in loss_dict.keys() if k in weight_dict)
-        loss_dict_reduced = utils.reduce_dict(loss_dict)
-        loss_dict_reduced_unscaled = {f'{k}_unscaled': v
-                                      for k, v in loss_dict_reduced.items()}
-        loss_dict_reduced_scaled = {k: v * weight_dict[k]
-                                    for k, v in loss_dict_reduced.items() if k in weight_dict}
-        losses_reduced_scaled = sum(loss_dict_reduced_scaled.values())
+        image = data_numpy.copy()
+        for mat in preds[0]:
+            x, y = int(mat[0]), int(mat[1])
+            cv2.circle(image, (x, y), 2, (255, 0, 0), 2)
 
-        loss_value = losses_reduced_scaled.item()
+            # vis result
+        cv2.imwrite("test_h36m.jpg", image)
+        cv2.imshow('res', image)
+        cv2.waitKey(10000)
 
-        return {'loss_value':loss}
-
-
-
-    
-
-    def validation_step(self, batch, batch_idx):
-        #metric_logger = utils.MetricLogger(delimiter="  ")
-        #metric_logger.add_meter('class_error', utils.SmoothedValue(window_size=1, fmt='{value:.2f}'))
-        #header = 'Test:'
-
-        iou_types = tuple(k for k in ('segm', 'bbox') if k in postprocessors.keys())
-        coco_evaluator = CocoEvaluator(base_ds, iou_types)
-
-
-    def test_step(self, batch, batch_idx):
-
-    
-
-    def get_optimizers(self):
-        optimizer = torch.optim.AdamW(param_dicts, lr=args.lr,
-                                  weight_decay=args.weight_decay)
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, args.lr_drop)
-        return [optimizer], [lr_scheduler]
-
-
-
-
+if __name__ == '__main__':
+    main()
